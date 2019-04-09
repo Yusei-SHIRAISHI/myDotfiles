26af4887cde1d81cf86b232441dd1afc98273d22
shirapi <yusei29game@gmail.com>
2019-04-10 01:01:20 +0900

    add .bashrc, .tmux.conf .vimrc install.sh

---
+++ .bashrc
@@ -0,0 +1,25 @@
+if [ $(uname) = 'Darwin' ];then
+    alias ls="ls -G"
+    if [ -f /usr/local/bin/gls ];then
+        alias ls="gls --color=auto"
+    fi
+else
+    alias ls="ls --color=auto"
+fi
+
+alias la="ls -a"
+alias lal="ls -la"
+alias sl="ls"
+alias ll="ls -l"
+
+alias poweroff="sudo shutdown -h now"
+
+psgrep() {
+    term=$(echo $1 | perl -pe "s/(.)(.*)/[\1]\2/")
+    ps -ef | grep ${term};
+}
+
+if [ -f /usr/local/bin/tmux ]; then
+    alias t="tmux";
+    alias tls="tmux ls";
+fi
---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#
+# install dotfiles
+#
+
+execdatetime=$(date +%Y-%m-%d-%H:%M:%S)
+currentDir=$(pwd)
+buckupDir=$HOME/.dotfiles.buckup
+[ -d ${backupDir} ] || mkdir -p ${backupDir};
+
+makeLinksToHomeDir(){
+  for x in `echo "$*"`; do
+        if [ -e ${HOME}/${x} ];then
+            cp -af  ${HOME}/${x} ${backupDir}/${x}.${execdatetime}.backup;
+        fi
+        ln -sfn ${currentDir}/${x} $HOME/${x}
+  done;
+}
+
+installList=('.bashrc' '.vimrc' 'tmux.conf')
+
+makeLinksToHomeDir ${installList[@]}
+
+exit 0;
